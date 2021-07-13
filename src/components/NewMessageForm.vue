f31f6b21466168f80fe85c54611d3e2786043219
jay <dj.min43@gmail.com>
2021-07-13 12:22:00 +0900

    component testing in progress

---
+++ src/components/NewMessageForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <input
+        type="text"
+        data-test="messageText"
+        v-model="inputText"
+        />
+        <button
+            data-test="sendButton"
+            @click="send"
+        >
+            Send
+        </button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "NewMessageForm",
+    data() {
+        return {
+            inputText: "",
+        }
+    },
+    methods: {
+        send() {
+            this.inputText = "";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
